e6480f893afb6c73345f193a278a2ccd1be5e79d
stuypend <andre@stuypend.com>
2021-02-24 13:38:44 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require "pry"
+
+class School
+
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade].push(name)
+  end
+
+  def grade(grade)
+    return @roster[grade]
+  end
+
+  def sort
+    @roster.each do
+      |grade, students|
+      #binding.pry
+      students.sort!
+    end
+    return @roster
+  end
+end
+
